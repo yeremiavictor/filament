78aabeaa1d5bd5b2c8650beed8711854be5253cd
yeremia victor <yeremiavictor@gmail.com>
2025-05-15 14:11:45 +0700

    add redirect setelah create edit

--- app/Filament/Resources/ProdukResource/Pages/EditProduk.php
+++ app/Filament/Resources/ProdukResource/Pages/EditProduk.php
@@ -16,4 +16,9 @@ class EditProduk extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+
+    protected function getRedirectUrl():string{
+        return $this->getResource()::getUrl('index');
+    }
 }
